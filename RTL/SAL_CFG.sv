1c93c737bd40f4ad4eeb52e2762669a098a13641
dale40 <dale40@gmail.com>
2023-01-03 13:33:37 +0900

    read data path (tentative)

--- RTL/SAL_CFG.sv
+++ RTL/SAL_CFG.sv
@@ -26,5 +26,6 @@ module SAL_CFG
     assign  sched_timing_if.t_ccd   = `T_CCD_VALUE;
     assign  sched_timing_if.t_wtr   = `T_WTR_VALUE;
     assign  sched_timing_if.t_rtw   = `T_RTW_VALUE;
+    assign  sched_timing_if.dfi_rden_lat = 4'd6;
 
 endmodule
